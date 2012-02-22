f69d8edad41b3c42cf9f300097ebc71db28db15f
sampwing <sampwing@gmail.com>
2012-02-21 23:11:23 -0800

    uploaded docs

---
+++ createdb.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sqlite3
+dbname = "portal.sqlite"
+createq = """CREATE TABLE Questions (qid INTEGER PRIMARY KEY AUTOINCREMENT,
+             question TEXT, answer TEXT)"""
+createu = """CREATE TABLE Users (phone VARCHAR(64) NOT NULL PRIMARY KEY, 
+             qid INTEGER, created FLOAT, finished FLOAT,
+             FOREIGN KEY(qid) REFERENCES Questions(qid))"""
+conn = sqlite3.connect(dbname)
+conn.execute(createq)
+conn.execute(createu)
+conn.commit()
+
+questions = "questions.data"
+qfd = open(questions, "r")
+for line in qfd:
+   try:
+      question, answer = eval(line)
+      conn.execute("INSERT INTO Questions(question, answer) VALUES(?, ?)", (question, answer))
+   except Exception, e: print e
+conn.execute("INSERT INTO Questions(qid, question, answer) VALUES(0, '', ?)", ("start begin lets go",))
+conn.commit()
+qfd.close()
---
+++ sms.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+import time
+import sqlite3
+import re
+import json
+import googlevoice as gv
+
+class QuestionHandler(object):
+   def __init__(self, dbname):
+      self.connection = sqlite3.connect(dbname)    
+      self.cursor = self.connection.cursor()
+      self.cursor.execute("SELECT MAX(Questions.qid) FROM Questions")
+      level, = self.cursor.fetchone()      
+      self.maxlevel = int(level)
+
+   def levelup(self, user):
+      self.cursor.execute("SELECT Users.qid FROM Users WHERE Users.phone=?", (user,))
+      currentlevel, = self.cursor.fetchone()
+      level = int(currentlevel)
+      print "Currently %d/%d level" % (level, self.maxlevel)
+      if level >= self.maxlevel:
+         return False # User is finished with the game
+      else:
+         self.cursor.execute("UPDATE Users SET qid=? WHERE phone=?", (level + 1, user,))
+         self.connection.commit()
+         return True # User is the next level
+         
+   def checkanswer(self, user, message):
+      question, answer = self.userinfo(user)
+      print "user:", user
+      print "sent:", message
+      print "correct answer:", answer
+      message_list = message.lower().split(' ')#re.findall(r"\w+", message)
+      if len(set(answer.lower().split(' ')).intersection(set(message_list))) > 0:
+         leveled = self.levelup(user)
+         if leveled:
+            question, answer = self.userinfo(user)
+            return "Correct! You can begin working on the next puzzle now! %s" % question
+         else:
+            self.cursor.execute("UPDATE Users SET finished=? WHERE phone=?", (time.time(), user))
+            self.connection.commit()
+            return "Congratulations! You have finished the game!"
+      else:
+         print message_list
+         print answer
+         return "Sorry, %s is not the correct answer for this question.  Please try again! %s" % (message, question)
+
+   def createuser(self, user):
+      self.cursor.execute("INSERT INTO Users(phone, qid, created) Values(?, 0, ?)", (user, time.time()))
+      self.connection.commit()
+
+   def userinfo(self, user):
+      self.cursor.execute("SELECT Questions.question, Questions.answer FROM Questions, Users WHERE Questions.qid = Users.qid AND Users.phone=?", (user,))
+      try:
+         question, answer = self.cursor.fetchone()
+         return question, answer
+      except Exception, e:
+         print e
+         self.createuser(user)
+         return self.userinfo(user)
+
+
+class Spawn(object):
+
+
+   def __init__(self, voice, db):
+      self.voice = voice
+      self.db = db
+
+   def checkmessages(self):
+      messages = self.voice.sms().messages
+      for message in messages:
+         if message.isRead == False:
+            value = self.readmessage(message)
+            print "check messages: ", value
+            if value == True:
+               message.mark()
+               message.delete()
+   
+   def readmessage(self, message):
+      user = message.phoneNumber
+      text = message.messageText
+      try:
+         reply = self.db.checkanswer(user, text)
+         self.voice.send_sms(user, reply)
+         print "SENDING: %s\nTO:%s" % (reply, user)
+         return True
+      except Exception, e:
+         print e
+         return False
+
+
+def main():
+   voice = gv.Voice()
+   username = ""
+   password = ""
+   voice.login(email=username, passwd=password)
+   db = QuestionHandler("portal.sqlite")
+   spawn = Spawn(voice, db)
+   while True:
+      spawn.checkmessages()
+      time.sleep(10)
+   
+
+if __name__ == '__main__':
+   main()
+
